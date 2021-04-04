3f81af8578b855fcd822df7a05632915f41216e2
xecarlox94 <jf94.uk@gmail.com>
2021-04-04 12:10:23 +0100

    adding more packages

--- scripts/.scripts/init_install.sh
+++ scripts/.scripts/init_install.sh
@@ -1,5 +1,5 @@
 
-APPS=(git vim neovim stow vifm g++ python3-pip zsh rpi-imager)
+APPS=(git vim neovim stow vifm g++ python3-pip zsh gparted apt-transport-https curl)
 
 source ~/.scripts/updt_upgr.sh
 
