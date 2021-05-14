a1fece059372d63e6cf1bab1c9eda84a1a191daf
xecarlox94 <jf94.uk@gmail.com>
2021-05-14 13:06:17 +0100

    improving deb installation

--- scripts/.scripts/deb/init_install_deb.sh
+++ scripts/.scripts/deb/init_install_deb.sh
@@ -8,7 +8,10 @@ do
 	sudo apt install -y "$APP"
 done
 
-source ~/.FILES/scripts/.scripts/deb
+
+
+source ~/.FILES/scripts/.scripts/deb/install_brave.sh
+source ~/.FILES/scripts/.scripts/deb/install_docker.sh
 
 source ~/.FILES/scripts/.scripts/init_python.sh
 source ~/.FILES/scripts/.scripts/init_nodejs.sh
--- scripts/.scripts/packages.sh
+++ scripts/.scripts/packages.sh
@@ -1,2 +1,1 @@
-
-APPS=(git vim neovim stow vifm g++ gcc python3-pip zsh apt-transport-https nodejs npm curl emacs ripgrep fd shellcheck docker make ranger)
+APPS=(git vim neovim stow vifm g++ gcc python3-pip zsh apt-transport-https nodejs npm curl emacs ripgrep fd shellcheck docker make ranger ca-certificates software-properties-common)
