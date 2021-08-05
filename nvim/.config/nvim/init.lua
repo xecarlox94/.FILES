a24623cc3abf9d2622eacd870737d50c78494166
xecarlox94 <jf94.uk@gmail.com>
2021-08-05 11:53:31 +0100

    merged vim and neovim configs properly

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,27 +1,9 @@
+-- Loading vimrc config
+vim.cmd 'source ~/.config/vim/vimrc'
+
 local o = vim.o -- global
 local wo = vim.wo -- window-local
 local bo = vim.bo -- buffer-local
 
-
-
--- set undodir=~/.cache/vim/undodir
--- set bg=dark
--- set tabstop=4 softtabstop=4
--- set shiftwidth=4
--- set signcolumn=yes
--- set guicursor=
-
 o.number = true
-o.relativenumber = true
-o.exrc = true
-o.hlsearch = false
-o.hidden  = true
-o.errorbells = false
-o.expandtab = true
-o.smartindent = true
-o.wrap = false
-o.swapfile = false
-o.backup = false
-o.undofile = true
-o.incsearch = true
-o.scrolloff = 4
+o.relativenumber = false
