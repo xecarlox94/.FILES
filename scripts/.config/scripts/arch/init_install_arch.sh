8c3712eb6a6d2af70f5fe32b8c1e536f8040e811
xecarlox94 <jf94.uk@gmail.com>
2021-08-16 14:29:27 +0100

    moving vim configuration to its location

--- scripts/.config/scripts/arch/init_install_arch.sh
+++ scripts/.config/scripts/arch/init_install_arch.sh
@@ -5,7 +5,7 @@ sudo pacman -Syu --no-confirm
 APPS+=(docker docker-compose python-pip ripgrep fd shellcheck)
 
 # desktop
-APPS+=(brave webkit2gtk gcr zathura zathura-pdf-poppler texlive-most gstreamer gst-plugins-good gst-libav)
+APPS+=(brave webkit2gtk gcr zathura zathura-pdf-poppler texlive-most gstreamer gst-plugins-good gst-libav sxiv)
 
 # virtual machine
 APPS+=(qemu ovmf vde2 ebtables dnsmasq bridge-utils openbsd-netcat qemu-arch-extra iptables-nft nftables)
---
+++ vim/.config/vim/init.vim
@@ -0,0 +1,28 @@
+"-------------------- SETTINGS
+
+source ~/.config/vim/settings.vim
+
+
+
+"-------------------- MAPPINGS
+
+source ~/.config/vim/mappings/mappings.vim
+
+
+
+"-------------------- AUTO COMMANDS
+
+source ~/.config/vim/autocommands.vim
+
+
+
+
+
+"-------------------- MOVE TO SETTINGS
+"
+"
+" work on colorscheme solution for vim and neovim
+"
+"
+"colorscheme molokai
+
