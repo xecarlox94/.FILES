1df2ed4fbebc4c00f5adab278c1302c8ee001a2b
xecarlox94 <jf94.uk@gmail.com>
2021-09-13 23:14:19 +0100

    fixing deb instalation script

--- scripts/.config/scripts/deb/init_install_deb.sh
+++ scripts/.config/scripts/deb/init_install_deb.sh
@@ -4,7 +4,11 @@ sudo apt update && sudo apt upgrade -y
 
 APPS+=(apt-transport-https python3-pip software-properties-common ca-certificates)
 
-sudo apt install -y "$APPS"
+for app in ${APPS[@]}
+do
+    sudo apt install -y $app
+done
+
 
 source ~/.FILES/scripts/.config/scripts/deb/install_brave.sh
 source ~/.FILES/scripts/.config/scripts/deb/install_docker.sh
