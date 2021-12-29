85f9325836c8004f148cbdbf20d2788dd7f04403
xecarlox94 <jf94.uk@gmail.com>
2021-12-29 15:03:52 +0000

    adding pushing all branches to remote

--- scripts/.config/scripts/commit.sh
+++ scripts/.config/scripts/commit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-[ $# -eq 1 ] && git status && git add . && git commit -m "$1" && git push || echo "ERROR"
+[ $# -eq 1 ] && git status && git add . && git commit -m "$1" && git push --all origin || echo "ERROR"
