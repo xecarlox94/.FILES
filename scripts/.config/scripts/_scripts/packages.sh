9fd61b08ade5f972df7105696f7d87529916dee4
xecarlox94 <jf94.uk@gmail.com>
2022-05-12 12:48:21 +0100

    modifying code

--- scripts/.config/scripts/_scripts/packages.sh
+++ scripts/.config/scripts/_scripts/packages.sh
@@ -1,6 +1,6 @@
 
-APPS=(gcc g++ git vim neovim stow vifm wget zsh nodejs npm emacs curl make tree tar htop zip unzip pkg-config cmake clang)
+APPS=(gcc g++ git vim neovim stow wget zsh nodejs npm emacs curl make tar zip unzip pkg-config cmake clang)
 
 #APPS+=(tor)
 
-APPS+=(virt-manager)
+#APPS+=(virt-manager)
