e0573f2a129ec7e32dadfdce009575250e1845a7
xecarlox94 <jf94.uk@gmail.com>
2022-08-28 19:00:51 +0100

    fixed issue with qutebrowser config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,24 +1,28 @@
+searx_instance="https://searxng.tordenskjold.de"
+invidious_instance="https://yewtu.be"
 
 
 config.load_autoconfig(False)
 
 
-
 c.url.start_pages = [
-    "https://searx.org/searchs",
+    searx_instance,
     "https://www.qutebrowser.org/doc/help/configuring.html#configpy"
 ]
 
-c.url.default_page="https://searx.org/searchs"
+
+c.url.default_page=searx_instance
+
 
 config.bind(",c", "config-edit")
 config.bind(",r", "restart")
+config.bind(",s", "open -t")
 
 
 
 c.url.searchengines = {
-    "DEFAULT": "https://searx.xyz/search?q={}",
-    "yt": "https://yewtu.be/results?search_query={}"
+    "DEFAULT": searx_instance + "/search?q={}",
+    "yt": invidious_instance + "/results?search_query={}"
 }
 
 
