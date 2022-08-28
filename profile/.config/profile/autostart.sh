9ef0f1201a2562778d59334f35449ea3a48547c2
xecarlox94 <jf94.uk@gmail.com>
2022-08-29 00:23:59 +0100

    removing startup logic from xmonad

--- profile/.config/profile/autostart.sh
+++ profile/.config/profile/autostart.sh
@@ -1,4 +1,8 @@
 #!/system/bin/sh
 
+exec set_wallpaper_theme.sh
+exec set_kb.sh
+
 
 exec xmonad
+#exec awesome
--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -6,10 +6,6 @@ import XMonad.Util.Run
 main :: IO()
 
 main = do
-    xmproc <- spawnPipe "xwallpaper --zoom Desktop/mountains_solitude_house_124060_1280x720.jpg"
-    xmproc <- spawnPipe "setxkbmap gb"
-    xmproc <- spawnPipe "wal -R"
-    xmproc <- spawnPipe "picom --experimental-backends"
     xmonad def {
         terminal    = "alacritty",
         borderWidth = 1
