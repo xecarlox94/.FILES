abb29c232705acc03160ac8baa1b722ec5cddda0
xecarlox94 <jf94.uk@gmail.com>
2022-12-26 12:27:33 +0000

    modifying arch init code

--- scripts/.config/scripts/_scripts/packages.sh
+++ scripts/.config/scripts/_scripts/packages.sh
@@ -1,6 +1,6 @@
 
-APPS=(gcc g++ git vim neovim stow wget zsh nodejs npm emacs curl make tar zip unzip pkg-config cmake clang)
+APPS=(gcc g++ git vim neovim stow wget zsh nodejs npm emacs curl make tar zip unzip pkg-config cmake clang qutebrowser)
 
-#APPS+=(tor)
+APPS+=(tor)
 
 APPS+=(virt-manager)
