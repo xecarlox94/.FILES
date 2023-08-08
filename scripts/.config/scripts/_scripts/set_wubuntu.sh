8ead8da624bb6d4dd1cfb394fae01bb0cf69eea8
xecarlox94 <jf94.uk@gmail.com>
2023-08-08 11:47:33 +0100

    adding setting wubuntu.sh

---
+++ scripts/.config/scripts/_scripts/set_wubuntu.sh
@@ -0,0 +1,6 @@
+
+
+
+xrandr --output eDP-1 --mode 2560x1600 --pos 1080x320 --rotate normal --primary
+xrandr --output DP-1 --mode 1080x1920 --pos 3640x0 --rotate right
+xrandr --output HDMI-1-0 --mode 1080x1920 --pos 0x0 --rotate right
