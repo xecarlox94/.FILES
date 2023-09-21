9cd3660f5aeeac02d1b9360b2f4faada95d1cd66
xecarlox94 <jf94.uk@gmail.com>
2023-09-21 10:41:53 +0100

    added X11

--- scripts/.config/scripts/_scripts/docker_run.sh
+++ scripts/.config/scripts/_scripts/docker_run.sh
@@ -17,10 +17,26 @@ DOCKER_NAME=$(echo $PWD | xargs basename | tr '[:upper:]' '[:lower:]' | sed "s/-
 DOCKER_NAME="$DOCKER_NAME:latest"
 
 
+# # --- Pulse Audio / Mic and Speakers - Too much to comment, but it's all needed... I think ----------------------
+#     -v /dev/snd:/dev/snd  \
+#     -v /run/user/$uid/puslse:/run/user/$uid/pulse \
+#     -v /dev/shm:/dev/shm \
+#     -v /etc/machine-id:/etc/machine-id \
+#     -v /var/lib/dbus:/var/lib/dbus \
+#     -v ~/.pulse:/home/$dockerUsername/.pulse \
+#     -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+#     -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+#     -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+#     --device /dev/snd \
+# # ----------------------------
+
 CMD="\
     sudo docker run \
     $DOCKER_ARGS \
     --gpus all --privileged \
+    -e DISPLAY=$DISPLAY \
+    -v /dev/bus/usb:/dev/bus/usb \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro
     -it --rm \
     $DOCKER_NAME \
     $RUN_CMD \
