f90087a432d371d1efcbb3c349a74726a7726ea8
xecarlox94 <jf94.uk@gmail.com>
2023-10-01 22:04:11 +0100

    testing install script

--- scripts/.config/scripts/conf/install.sh
+++ scripts/.config/scripts/conf/install.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
+
 BACKUP_DIR=$HOME/.config/backup
-
-
 mkdir -p $BACKUP_DIR
 
-mv $HOME/.bashrc    $BACKUP_DIR
-mv $HOME/.profile   $BACKUP_DIR
+
+#mv $HOME/.bash_profile   $BACKUP_DIR
+#mv $HOME/.bash_logout   $BACKUP_DIR
+
+
+mv $HOME/.bashrc        $BACKUP_DIR
+mv $HOME/.profile       $BACKUP_DIR
 
 
 $HOME/.config/.FILES/scripts/.config/scripts/conf/stow.sh
 
+
 bash
-
-
