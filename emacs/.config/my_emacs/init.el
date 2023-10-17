9be7e4662a898a06cfa32442ea6e4e06f26be761
xecarlox94 <jf94.uk@gmail.com>
2023-10-17 14:11:13 +0100

    new scripts

--- emacs/.config/my_emacs/init.el
+++ emacs/.config/my_emacs/init.el
@@ -466,3 +466,15 @@
       ("https://yewtu.be/feed/channel/UCPsCJ1j0G45FnRGqJhCHLiA" finance economy bitcoin)
       ))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
