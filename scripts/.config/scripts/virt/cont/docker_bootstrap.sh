3f38c7e514518a36f16f41c7e8d3523c47e7414a
xecarlox94 <jf94.uk@gmail.com>
2023-10-19 16:34:04 +0100

    added extra config

--- scripts/.config/scripts/virt/cont/docker_bootstrap.sh
+++ scripts/.config/scripts/virt/cont/docker_bootstrap.sh
@@ -1,26 +1,60 @@
 
 echo "please insert the project name: "
-
 read PROJECT_FOLDER
+
+echo "please insert the base docker image name: "
+read DOCKER_IMAGE_NAME
+
+
+X11=false
+while true;
+do
+    read -p "Do you wish to run this container with X11 Desktop session? (y/n)" yn
+    case $yn in
+        [Yy]* ) X11=true; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no."
+    esac
+done
+
+NVIDIA=false
+while true;
+do
+    read -p "Do you wish to run this container with Nvidia runtime? (y/n)" yn
+    case $yn in
+        [Yy]* ) NVIDIA=true; break;;
+        [Nn]* ) break;;
+    esac
+done
+
+
+ADD_OPTS=""
+
+if $X11;
+then
+    ADD_OPTS="-x "
+fi
+
+
+if $NVIDIA;
+then
+    ADD_OPTS="$ADD_OPTS -n"
+fi
+
 
 mkdir_cd $PROJECT_FOLDER
 
-echo "please insert the base docker image name: "
-
-read DOCKER_IMAGE_NAME
-
-touch Dockerfile run.sh
 
 echo -e "\
 FROM $DOCKER_IMAGE_NAME \n\
 WORKDIR /src \
     " > Dockerfile
-
 mkdir src
 
-echo "sudo to create run.sh executable file"
 
+echo "sudo to create run.sh executable file"
 touch_x run.sh
+
 
 echo -e "\
 docker_run.sh \\
@@ -29,7 +63,8 @@ docker_run.sh \\
     \"\\
     \"\\
         -v \${PWD}/src:/src \\
-    \"" > run.sh
+    \"\\
+    $ADD_OPTS" > run.sh
 
 
 docker_build.sh
--- scripts/.config/scripts/virt/cont/docker_run.sh
+++ scripts/.config/scripts/virt/cont/docker_run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 
-if [ $# -lt 1 ];
+if [ $# -lt 2 ];
 then
     echo "not enough args"
     exit 1
 fi
+
 
 
 RUN_CMD="$1"
@@ -13,7 +14,37 @@ RUN_CMD="$1"
 DOCKER_ARGS="$2"
 
 
+shift 2
+
+
+X11=false
+NVIDIA=false
+
+while getopts 'xn' OPTION;
+do
+    case "$OPTION" in
+        x)
+            echo "X11 is true"
+            X11=true
+            ;;
+        n)
+            echo "NVIDIA is true"
+            NVIDIA=true
+            ;;
+        *)
+            echo "NOTHING"
+            ;;
+    esac
+done
+
+
+
 DOCKER_NAME=$(get_container_name.sh)
+
+
+#echo "__DOCKER_CONTAINER_NAME__: $DOCKER_NAME"
+
+
 
 
 # # --- Pulse Audio / Mic and Speakers - Too much to comment, but it's all needed... I think ----------------------
@@ -25,6 +56,7 @@ DOCKER_NAME=$(get_container_name.sh)
 #     -v ~/.pulse:/home/$dockerUsername/.pulse \
 #     -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
 #     -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+#     -v /dev/bus/usb:/dev/bus/usb \
 #     -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
 #     --device /dev/snd \
 # # ----------------------------
@@ -35,14 +67,6 @@ DOCKER_NAME=$(get_container_name.sh)
     # --user \"$(id -u):$(id -g)\" \
 
 
-
-# COPY --from=glvnd /usr/share/glvnd/egl_vendor.d/10_nvidia.json /usr/share/glvnd/egl_vendor.d/10_nvidia.json
-# ENV NVIDIA_VISIBLE_DEVICES ${NVIDIA_VISIBLE_DEVICES:-all}
-# ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES:-all}
-
-#VIM CONFIG
-#-v .config/vim/init.vim:~/.vimrc \
-
 #-e TERM \
     #-e QT_X11_NO_MITSHM=1 \
     #-e XAUTHORITY=/tmp/.dockerw_b717qf.xauth \
@@ -50,33 +74,51 @@ DOCKER_NAME=$(get_container_name.sh)
     #-v /tmp/.X11-unix:/tmp/.X11-unix \
 
 
-X11_NVIDIA="\
-    -e TERM \
-    -e DISPLAY=unix$DISPLAY \
-    -e NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-all} \
-    -e NVIDIA_DRIVER_CAPABILITIES=${NVIDIA_DRIVER_CAPABILITIES:-all} \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    --runtime=nvidia \
-    --gpus all \
+
+X11_OPTS="\
+-e TERM \
+-e DISPLAY=unix$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+"
+
+NVIDIA_OPTS="\
+-e NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-all} \
+-e NVIDIA_DRIVER_CAPABILITIES=${NVIDIA_DRIVER_CAPABILITIES:-all} \
+--runtime=nvidia \
+--gpus all \
 "
 
 
-XHOST="xhost +local:root && "
+XHOST=""
+ADD_OPTS=""
+
+
+echo "X11: $X11"
+echo "NVIDIA: $NVIDIA"
+
+if $X11;
+then
+    XHOST="xhost +local:root && "
+    ADD_OPTS="$X11_OPTS "
+fi
+
+
+if $NVIDIA;
+then
+    ADD_OPTS="$ADD_OPTS $NVIDIA_OPTS"
+fi
 
 CMD="\
-    sudo docker run \
-    -it \
-    -v $HOME/.config/.FILES:/root/.config/.FILES \
-    $DOCKER_ARGS \
-    -v /dev/bus/usb:/dev/bus/usb \
-    $X11_NVIDIA \
-    $DOCKER_NAME \
-    $RUN_CMD \
+$XHOST \
+sudo docker run \
+-it \
+-v $HOME/.config/.FILES:/root/.config/.FILES \
+$DOCKER_ARGS \
+$ADD_OPTS \
+$DOCKER_NAME \
+$RUN_CMD \
 "
 
-#CMD="$XHOST && $CMD"
+#echo "$CMD"
 
-echo "__DOCKER_CONTAINER_NAME__: $DOCKER_NAME"
-
-eval "$XHOST $CMD"
-
+eval "$CMD"
