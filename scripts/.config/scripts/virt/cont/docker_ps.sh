5ed5ad63bbf76470f78a6fe0ea7fb79f6a52b418
matthew <matt2410@live.com>
2023-10-31 12:03:13 +0000

    refactoring docker ps

--- scripts/.config/scripts/virt/cont/docker_ps.sh
+++ scripts/.config/scripts/virt/cont/docker_ps.sh
@@ -1,1 +1,10 @@
-sudo docker ps --format="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n"
+sudo docker ps \
+    --format="\
+ID\t{{.ID}}\n\
+NAME\t{{.Names}}\n\
+IMAGE\t{{.Image}}\n\
+PORTS\t{{.Ports}}\n\
+COMMAND\t{{.Command}}\n\
+CREATED\t{{.CreatedAt}}\n\
+STATUS\t{{.Status}}\n\n\
+"
