051d3dca198248a0c39ac22a1c38d37a6bed88ac
xecarlox94 <jf94.uk@gmail.com>
2024-06-07 19:50:25 +0100

    declaring different interfaces for user interaction

--- config_module/Main.hs
+++ config_module/Main.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p "haskellPackages.ghcWithPackages (p: with p; [turtle])" -i ghci
+#!nix-shell -p "haskellPackages.ghcWithPackages (p: with p; [turtle])" -i runhaskell
 
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -22,10 +22,15 @@ main = do
 -- Desktop and shell Keybindings
 
 
+-- (leader key/key combination) for program
+-- key combination
+--      splitV -> i
+--      splitV -> i
+
 
 class WindowSplit a where
-    splitUp     :: Text -> Text
-    splitDown   :: Text -> Text
+    splitV :: Text -> Text
+    splitH :: Text -> Text
 
 class WindowMovement a where
     moveUp      :: Text -> Text
@@ -40,21 +45,35 @@ class WindowFocus a where
     focusRight  :: Text -> Text
 
 class WindowResize a where
-    increaseVertically      :: Text -> Text
-    increaseHorizontally    :: Text -> Text
-    decreaseVertically      :: Text -> Text
-    decreaseHorizontally    :: Text -> Text
+    increaseV :: Text -> Text
+    increaseH :: Text -> Text
+    decreaseV :: Text -> Text
+    decreaseH :: Text -> Text
 
+
+class Zoomable a where
+    zoomIn  :: Text -> Text
+    zoomOut :: Text -> Text
 
 
 class Buffer a where
-    new
-    switch
-    open
-    edit
+    new         :: Text -> Text
+    switch      :: Text -> Text -- Switchable class ??
+    open        :: Text -> Text
+    find        :: Text -> Text
+    edit        :: Text -> Text
+    sudoEdit    :: Text -> Text
+
+
+class FileSystemControl a where
+    open        :: Text -> Text
+
+class FileSystemNav a where
+    goParent    :: Text -> Text
+    goInDir     :: Text -> Text
 
 -- buffers new, switch, open, edit, save, close
-    -- file find, new, open, edit, save, close
+-- file find, new, open, edit, save, close
 -- tab new, switch, open, edit, save, close
 
 -- plugins telescope ...
