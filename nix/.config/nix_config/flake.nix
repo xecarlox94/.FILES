473d013f92951e5cb3002d6b41752498c1786ab6
xecarlox94 <jf94.uk@gmail.com>
2024-12-18 23:29:01 +0000

    working on nix config

--- nix/.config/nix_config/flake.nix
+++ nix/.config/nix_config/flake.nix
@@ -3,19 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    helix.url = "github:helix-editor/helix/master";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }@inputs: {
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; } ;
       modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-
         /etc/nixos/configuration.nix
       ];
     };
+
+    # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-configuration-explained
 
     # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
