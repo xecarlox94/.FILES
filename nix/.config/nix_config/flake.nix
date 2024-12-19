137ce6f7d44f31600d3ec0b88c2aeca6126b872b
xecarlox94 <jf94.uk@gmail.com>
2024-12-19 17:45:21 +0000

    small fix on nix flake

--- nix/.config/nix_config/flake.nix
+++ nix/.config/nix_config/flake.nix
@@ -2,13 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = { nixpkgs, home-manager, ... }: {
@@ -17,25 +11,11 @@
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          /etc/nixos/configuration.nix
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-
-            home-manager.users.xecarlox = import ./home.nix;
-          }
+          ./configuration.nix
 
         ];
       };
     };
-
-    # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-configuration-explained
-
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
   };
 }
