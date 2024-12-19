9c934a1da8254f297ca897d67e5771e45989c07d
xecarlox94 <jf94.uk@gmail.com>
2024-12-19 16:16:42 +0000

    working on nix config

--- nix/.config/nix_config/flake.nix
+++ nix/.config/nix_config/flake.nix
@@ -4,17 +4,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    helix.url = "github:helix-editor/helix/master";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, ... }: {
 
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; } ;
-      modules = [
-        /etc/nixos/configuration.nix
-      ];
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          /etc/nixos/configuration.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.xecarlox = import ./home.nix;
+          }
+
+        ];
+      };
     };
 
     # https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-configuration-explained
