a844f31ea7086202af58380d90578f0099a42f64
xecarlox94 <jf94.uk@gmail.com>
2024-12-19 18:27:12 +0000

    working on home manager config

--- nix/.config/home-manager/flake.nix
+++ nix/.config/home-manager/flake.nix
@@ -3,16 +3,26 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    helix.url =  "github:helix-editor/helix/master";
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, ... }@inputs: {
 
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
 
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.xecarlox = import ./home.nix;
+          }
         ];
       };
     };
