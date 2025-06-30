187ae4ed039581cdd57e14627f9d6dcd7e7becd9
xecarlox94 <jf94.uk@gmail.com>
2025-06-30 19:47:54 +0100

    fixed inlay hints issue

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -43,46 +43,24 @@ in
     ];
 
     plugins = {
+
+      bacon.enable = true;
+      conform-nvim = {
+        enable = true;
+        settings = {formatters_by_ft.rust = ["rustfmt"];};
+      };
+      # dap-lldb.enable = true;
       rustaceanvim = {
         enable = true;
         settings = {
-          server = {
-            dap.adapters.lldb = {
-              type = "server";
-              port = "${''$''}{port}";
-              executable = {
-                command = "codelldb";
-                args = [
-                  "--port"
-                  "${''$''}{port}"
-                ];
-              };
-            };
-          };
           tools.enable_clippy = true;
           server = {
             default_settings = {
-              inlayHints = {
-                lifetimeElisionHints = {
-                  enable = "always";
-                };
-              };
+              inlayHints = {lifetimeElisionHints = {enable = "always";};};
               rust-analyzer = {
-                cargo = {
-                  allFeatures = true;
-                };
-                check = {
-                  command = "clippy";
-                };
-                files = {
-                  excludeDirs = [
-                    "target"
-                    ".git"
-                    ".cargo"
-                    ".github"
-                    ".direnv"
-                  ];
-                };
+                cargo = {allFeatures = true;};
+                check = {command = "clippy";};
+                files = {excludeDirs = ["target" ".git" ".cargo" ".github" ".direnv"];};
               };
             };
           };
@@ -253,6 +231,7 @@ in
       lsp = {
         enable = true;
 
+        inlayHints = true;
         # inlayHints.enable = true;
 
         keymaps = {
