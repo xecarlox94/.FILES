796557205ea3a5bc693340c85f368d562e015c60
xecarlox94 <jf94.uk@gmail.com>
2025-07-15 16:10:32 +0100

    changing to gnome display manager in thinkcenter

--- machines/thinkcenter/configuration.nix
+++ machines/thinkcenter/configuration.nix
@@ -45,9 +45,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the XFCE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.xfce.enable = true;
+  # Desktop Environment.
+  displayManager = {
+    gdm.enable = true;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -136,12 +137,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
 }
